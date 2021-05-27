be46e1d4411baa5691d2982b3594bee12154ec88
Anoubis <mail@hiddentribe.ru>
2021-05-27 11:05:34 +0300

    Update M.GetUDFs.m

--- Misc/M.GetUDFs.m
+++ Misc/M.GetUDFs.m
@@ -1,6 +1,10 @@
 let
-  Source = Record.FieldNames(#shared),
-  UDF    = List.Select(Source, each Record.HasFields(#sections[Section1], _)),
-  Result = List.Buffer(UDF)
+  Source = Record.FieldNames(#shared), 
+  UDF = List.Select(Source, each Record.HasFields(#sections[Section1], _)), 
+  FilteredFunctions = List.Select(
+    UDF, 
+    each not Text.StartsWith(_, "fn") and not Text.StartsWith(_, "M_")
+  ), 
+  Result = List.Buffer(FilteredFunctions)
 in
   Result
